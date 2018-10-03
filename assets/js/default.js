3b5debfe4909bee3368fd04f0ef6b1b079e434b2
amastin-devicepitstop <43486447+amastin-devicepitstop@users.noreply.github.com>
2018-10-03 16:28:26 -0500

    Update default.js

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,26 +1,41 @@
 let database;
 
 window.onload = function(){
-  console.log($(window).width());
-  console.log($(window).height());
-  
-  let animation = document.getElementById("load-screen");
-  animation.style.display = "none";
-  
+  hideAnimation();
+  initDatabase();
+  preventFormSubmit();
+
+}
+
+function initDatabase(){
   const config = {
-    apiKey: "AIzaSyAchMHl30dBGYZHfVJR3ch12jFCpReQSdM",
-    authDomain: "devicepitstop-imsdb.firebaseapp.com",
-    databaseURL: "https://devicepitstop-imsdb.firebaseio.com",
-    projectId: "devicepitstop-imsdb",
-    storageBucket: "devicepitstop-imsdb.appspot.com",
-    messagingSenderId: "658605927128"
-  };
-  const app = firebase.initializeApp(config);
-  const settings = {
-    timestampsInSnapshots: true  
-  };
-  database = firebase.firestore();
-  database.settings(settings);
+      apiKey: "AIzaSyAchMHl30dBGYZHfVJR3ch12jFCpReQSdM",
+      authDomain: "devicepitstop-imsdb.firebaseapp.com",
+      databaseURL: "https://devicepitstop-imsdb.firebaseio.com",
+      projectId: "devicepitstop-imsdb",
+      storageBucket: "devicepitstop-imsdb.appspot.com",
+      messagingSenderId: "658605927128"
+    };
+    const app = firebase.initializeApp(config);
+    const settings = {
+      timestampsInSnapshots: true  
+    };
+    database = firebase.firestore();
+    database.settings(settings);
+  }  
+}
+
+function hideAnimation() {
+  document.getElementById("load-screen").style.display = "none";
+}
+
+function preventFormSubmit() {
+  if (window.location.href.indexOf("new") !== -1){
+    $("form").submit(function (e) {  
+      e.preventDefault();
+      addStoreStock();
+    });	
+  }
 }
 
 function save(collection, doc, subCollection, subDoc, json) {
