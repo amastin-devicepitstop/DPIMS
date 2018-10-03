d052365835c8273737d9fd88538cbd68cfd98394
amastin-devicepitstop <43486447+amastin-devicepitstop@users.noreply.github.com>
2018-10-03 15:42:13 -0500

    Update default.js

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,4 +1,21 @@
+let database;
+
 window.onload = function(){
   let animation = document.getElementById("load-screen");
   animation.style.display = "none";
-} 
+  
+  const config = {
+    apiKey: "AIzaSyAchMHl30dBGYZHfVJR3ch12jFCpReQSdM",
+    authDomain: "devicepitstop-imsdb.firebaseapp.com",
+    databaseURL: "https://devicepitstop-imsdb.firebaseio.com",
+    projectId: "devicepitstop-imsdb",
+    storageBucket: "devicepitstop-imsdb.appspot.com",
+    messagingSenderId: "658605927128"
+  };
+  const app = firebase.initializeApp(config);
+  const settings = {
+    timestampesInSnapshots: true  
+  };
+  database = firebase.firestore();
+  database.settings(settings);
+}
