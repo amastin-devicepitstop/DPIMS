74356ed5941350e78d618f7019cdae9479d5b5ee
amastin-devicepitstop <43486447+amastin-devicepitstop@users.noreply.github.com>
2018-10-03 16:17:08 -0500

    Update default.js

--- assets/js/default.js
+++ assets/js/default.js
@@ -45,3 +45,14 @@ function get(collection, doc) {
         console.log("Error getting document:", error);
     });
 }
+
+function addStoreStock() {
+  let name = document.getElementById("first-name").value + " " + document.getElementById("last-name").value;
+  let manufacturer = document.getElementById("manufacturer").value;
+  let model = document.getElementById("model").value;
+  let actions = document.getElementById("actions").value;
+  let sku = document.getElementById("sku").value;
+  let date = document.getElementById("date").value;
+  
+  save("devices", manufacturer, model, sku, {tech: name, actions: actions, date: date});
+}
