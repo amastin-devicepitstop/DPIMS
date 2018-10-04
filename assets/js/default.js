ec556dbe4e27fc4355cce0ab246499bfd186ca7a
amastin-devicepitstop <43486447+amastin-devicepitstop@users.noreply.github.com>
2018-10-04 16:22:52 -0500

    Update default.js

--- assets/js/default.js
+++ assets/js/default.js
@@ -55,7 +55,7 @@ function get(collection, doc) {
 }
 
 function getWhere(collection) {
-  database.collection(collection).where("iPhone 6", "==", "iPhone 6").get()
+  database.collection(collection).where("Apple", "==", "iPhone 6").get()
     .then(function(querySnapshot) {
         querySnapshot.forEach(function(doc) {
             // doc.data() is never undefined for query doc snapshots
