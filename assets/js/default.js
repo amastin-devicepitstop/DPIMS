38bae3fd6af18bd74dd2e541697ab9930dab1501
amastin-devicepitstop <43486447+amastin-devicepitstop@users.noreply.github.com>
2018-10-04 16:47:43 -0500

    Update default.js

--- assets/js/default.js
+++ assets/js/default.js
@@ -68,11 +68,12 @@ function getWhere(collection) {
 }
 
 function getAll(collection) {
-  database.collection("cities").get().then(function(querySnapshot) {
+  database.collection(collection).get().then(function(querySnapshot) {
     querySnapshot.forEach(function(doc) {
         // doc.data() is never undefined for query doc snapshots
         console.log(doc.id, " => ", doc.data());
     });
+});
 }
 
 // ===============
