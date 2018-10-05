1f9fa6dfb82d602f94a479e9763d3d77901c62e5
amastin-devicepitstop <43486447+amastin-devicepitstop@users.noreply.github.com>
2018-10-05 17:23:57 -0500

    Update storestock.js

--- assets/js/storestock.js
+++ assets/js/storestock.js
@@ -10,5 +10,40 @@ window.onload = function(){
 
 function getStoreStock() {
   results = getWhere("devices", "month", "==", "10");
-  console.log(results);
+  
+  for (let i = 0; i < results.length; i++){
+    let product = results[i];
+    addStoreStockRow(product.tech, product.manufacturer, product.model, product.actions, product.sku, product.date);
+  }
+}
+
+function addStoreStockRow(tech, manufacturer, model, actions, sku, date) {
+  $("#store-stock-tracker > tbody").append("<tr> +
+                                      "<td> +
+                                        "<input type='checkbox'> +
+                                      "</td> +
+                                      "<td class='overflow'> + 
+                                        tech + 
+                                      "</td> +
+                                     
+                                      "<td class='overflow'> + 
+                                        manufacturer + 
+                                      "</td> +
+                                     
+                                      "<td class='overflow'> + 
+                                        model + 
+                                      "</td> +
+                                     
+                                      "<td class='overflow'> + 
+                                        actions + 
+                                      "</td> +
+                                     
+                                      "<td class='overflow'> + 
+                                        sku + 
+                                      "</td> +
+                                     
+                                      "<td class='overflow'> + 
+                                        date + 
+                                      "</td> +
+                                     </tr>");
 }
