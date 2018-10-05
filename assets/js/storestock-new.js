d14cd76e4c192eae6d1f7c90c20c4d1ad35b71d1
amastin-devicepitstop <43486447+amastin-devicepitstop@users.noreply.github.com>
2018-10-05 16:29:09 -0500

    Update storestock-new.js

--- assets/js/storestock-new.js
+++ assets/js/storestock-new.js
@@ -1,1 +1,26 @@
+function preventFormSubmit() {
+  // Check that the current page is the "New Store Stock" page
+  
+  if (urlContains("new")) {
+    $("#date").val(new Date().toLocaleDateString());
+    
+    // Prevent form submission
+    $("form").submit(function (e) {  
+      e.preventDefault();
+      addStoreStock();
+    });	
+  }
+}
+
+function addStoreStock() {
+  let name = document.getElementById("first-name").value + " " + document.getElementById("last-name").value;
+  let manufacturer = document.getElementById("manufacturer").value;
+  let model = document.getElementById("model").value;
+  let actions = document.getElementById("actions").value;
+  let sku = document.getElementById("sku").value;
+  let date = document.getElementById("date").value;
+  
+  //save("devices", manufacturer, model, sku, {tech: name, actions: actions, date: date});
+  save("devices", sku, {tech: name, manufacturer: manufacturer, model: model, actions: actions, sku: sku, date: date});
+}
 
