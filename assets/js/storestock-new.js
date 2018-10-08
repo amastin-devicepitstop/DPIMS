f8e435d3b073c0c40d3c26dda39e10109287cd44
amastin-devicepitstop <43486447+amastin-devicepitstop@users.noreply.github.com>
2018-10-08 10:20:13 -0500

    Update storestock-new.js

--- assets/js/storestock-new.js
+++ assets/js/storestock-new.js
@@ -37,8 +37,10 @@ function addAction() {
   let actions = document.getElementById("actions");
   let selectedOption = document.getElementById("actions-performed").value;
   
-  console.log(actions);
-  console.log(document.getElementById("actions"));
+  console.log(actions.text);
+  console.log(actions.value);
+  console.log(actions.innerText);
+  console.log(actions.innerHTML);
   
   // If input is empty...
   if (actions.text == "") {
