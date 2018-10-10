601638348c6a507ccc3cedacc56e3d9d96245cc9
amastin-devicepitstop <43486447+amastin-devicepitstop@users.noreply.github.com>
2018-10-10 00:13:19 -0500

    Update storestock.js

--- assets/js/storestock.js
+++ assets/js/storestock.js
@@ -15,8 +15,7 @@ function getStoreStock() {
     
   setTimeout(function(){ // works
     if (query.length > 0) {
-      console.log("TRUE");
-      $("#selectAll").html("<input type='checkbox' class='selectAll'>")
+      $(".selectAll").removeAttr("disabled");
     }
     for (let i = 0; i < query.length; i++){
       let product = query[i];
