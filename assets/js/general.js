87599b9bc6d9b935d4c517249e9b4a058a8fa5f8
amastin-devicepitstop <43486447+amastin-devicepitstop@users.noreply.github.com>
2018-10-11 10:48:41 -0500

    Update general.js

--- assets/js/general.js
+++ assets/js/general.js
@@ -73,7 +73,7 @@ function initDatabase(){
   database.settings(settings);
 }  
 
-function del(collection, doc) {
+function remove(collection, doc) {
   database.collection(collection).doc(doc).delete().then(function() {
     console.log("Document successfully deleted!");
   }).catch(function(error) {
