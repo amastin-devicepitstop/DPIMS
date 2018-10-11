53b583269fcc68da3106ec571434a0af03a78093
amastin-devicepitstop <43486447+amastin-devicepitstop@users.noreply.github.com>
2018-10-11 01:34:10 -0500

    Update storestock-edit.js

--- assets/js/storestock-edit.js
+++ assets/js/storestock-edit.js
@@ -8,6 +8,7 @@ window.onload = function(){
 
 function populateForm(query) {
   let product = query[0];
+  console.log(product.tech.split(" "));
   let firstName = product.tech.split(" ")[0];
   let lastName product.tech.split(" ")[1];
   let manufacturer = product.manufacturer;
