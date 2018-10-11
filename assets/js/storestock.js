3e46b784e2b3638f41f555528b9786d0670fa0c8
amastin-devicepitstop <43486447+amastin-devicepitstop@users.noreply.github.com>
2018-10-10 22:47:39 -0500

    Update storestock.js

--- assets/js/storestock.js
+++ assets/js/storestock.js
@@ -15,10 +15,11 @@ function getStoreStock() {
     
   setTimeout(function(){ // works
     enableSelectAll(query);
+    hideAnimation();
     populateTable(query);
     // Add a check here -- if query length > 0, check if data has been added to the table, and if not, run query again until it has been added.
     initCheckboxes();
-    hideAnimation();
+    //hideAnimation();
   }, 1000); 
 }
 
