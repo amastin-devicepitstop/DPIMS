5f4503ba82f9617749bfaed0a96faf90a0b84396
amastin-devicepitstop <43486447+amastin-devicepitstop@users.noreply.github.com>
2018-10-11 11:27:43 -0500

    Update general.js

--- assets/js/general.js
+++ assets/js/general.js
@@ -8,7 +8,22 @@ window.onload = function(){
 // ===============
 // === GENERAL ===
 // ===============
-
+function showConfirmDialog(message) {
+//   let modalScreen = "<div class='modal-open'>";
+//   let modalBackdrop = "<div class='modal-backdrop fade  in'></div>";
+//   let modal = "<div class='modal fade show in'>";
+//   let modalDialog = "<div class='modal-dialog modal-sm'>";
+//   let modalContent = "<div class='modal-content'>"
+//   let modalBody = "<div class='modal-body'>";
+//   let row = "<div class='row'>";
+//   let iconContainer = "<div class=''>";
+//   let icon = "<i><svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512' class='icon icon-xxlg-md attention-circle-bg'><path d='M256 32c30.3 0 59.6 5.9 87.2 17.6 26.7 11.3 50.6 27.4 71.2 48s36.7 44.5 48 71.2c11.7 27.6 17.6 56.9 17.6 87.2s-5.9 59.6-17.6 87.2c-11.3 26.7-27.4 50.6-48 71.2s-44.5 36.7-71.2 48C315.6 474.1 286.3 480 256 480s-59.6-5.9-87.2-17.6c-26.7-11.3-50.6-27.4-71.2-48s-36.7-44.5-48-71.2C37.9 315.6 32 286.3 32 256s5.9-59.6 17.6-87.2c11.3-26.7 27.4-50.6 48-71.2s44.5-36.7 71.2-48C196.4 37.9 225.7 32 256 32m0-32C114.6 0 0 114.6 0 256s114.6 256 256 256 256-114.6 256-256S397.4 0 256 0z'></path><circle cx='256' cy='384' r='32'></circle><path d='M256.3 96.3h-.6c-17.5 0-31.7 14.2-31.7 31.7v160c0 17.5 14.2 31.7 31.7 31.7h.6c17.5 0 31.7-14.2 31.7-31.7V128c0-17.5-14.2-31.7-31.7-31.7z'></path></svg></i>"
+//   let endDiv = "</div>"
+  
+//   $("#load-screen").after(modalScreen + modalBackdrop + modal + modalDialog + modalContent + modalBody);
+  
+//   $("#load-screen").after("</div> <div class='col-md-10 col-sm-10 col-xs-10' style='padding-left: 2px'><p>Do you want to delete the selected contacts?</p> <!----><!----> <div class='alert-actions btn-toolbar'> <button class='btn btn-primary’ type='submit'> OK </button> <button class='btn btn-default ' > Cancel </button></div></div></div></div></div></div></div></div>")  
+}
 
 function hideAnimation() {
   document.getElementById("load-screen").style.display = "none";
