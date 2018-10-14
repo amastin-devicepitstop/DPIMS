715ec1add3879a3ab72f3ede15c6efa2fede4529
amastin-devicepitstop <43486447+amastin-devicepitstop@users.noreply.github.com>
2018-10-14 01:08:51 -0500

    Update storestock.js

--- assets/js/storestock.js
+++ assets/js/storestock.js
@@ -109,12 +109,14 @@ function initCheckboxes() {
 
 function shadeSelected() {
   // Any <tr> elements should have a white background if it's checkbox is not checked
-  $("input:checkbox:not(:checked)").closest('tr').css('background-color', '#ffffff');
+  $("input:checkbox:not(:checked)").closest('tr').toggleClass('tr-selected', 'false');
+//   $("input:checkbox:not(:checked)").closest('tr').css('background-color', '#ffffff');
   
   // Any <tr> elements should have a background color of #f0f0f0 if it's checkbox is checked
   // Additionally the bottom border color should change so it doesn't blend in to the background.
-  $("input:checkbox:checked").closest('tr').css('background-color', '#f0f0f0');
-  $("input:checkbox:checked").closest('tr').css('border-bottom', '1px solid #e5e5e5');
+//   $("input:checkbox:checked").closest('tr').css('background-color', '#f0f0f0');
+//   $("input:checkbox:checked").closest('tr').css('border-bottom', '1px solid #e5e5e5');
+  $("input:checkbox:checked").closest('tr').toggleClass('tr-selected', 'true');
 }
 
 function markAsSold() {
