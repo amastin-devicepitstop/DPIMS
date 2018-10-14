17bacd55fd300ae6b3900061d3b0facd5a766345
amastin-devicepitstop <43486447+amastin-devicepitstop@users.noreply.github.com>
2018-10-14 01:33:14 -0500

    Update storestock-new.js

--- assets/js/storestock-new.js
+++ assets/js/storestock-new.js
@@ -31,7 +31,10 @@ function addStoreStock() {
   let day = getDay(date);
   let year = getYear(date);
   
-  save("devices", sku, {tech: name, manufacturer: manufacturer, model: model, actions: actions, sku: sku, date: date, month: month, day: day, year: year});
+  let sold = false;
+  let ready = false;
+  
+  save("devices", sku, {tech: name, manufacturer: manufacturer, model: model, actions: actions, sku: sku, date: date, month: month, day: day, year: year, sold: sold, ready: ready});
 }
 
 function addAction() {
