3074cadd83993a15142268102c383f7d6e4aa5a3
amastin-devicepitstop <43486447+amastin-devicepitstop@users.noreply.github.com>
2018-10-16 16:49:38 -0500

    Update sign-maker.js

--- assets/js/sign-maker.js
+++ assets/js/sign-maker.js
@@ -28,7 +28,7 @@ function addSign() {
   let sku = $("#sku").val();
 
   let signBegin = "<div class='sign-preview'><div><table><tbody>";
-  let logoAndPriceRow = "<tr><td class='dp-logo sign-logo'></td><td class='sign-cell'><input class='sign-price'>" + '$' + price + "</input></td></tr>";
+  let logoAndPriceRow = "<tr><td class='dp-logo sign-logo'></td><td class='sign-cell'><input class='sign-price sign-value' value='$'" + price + "></td></tr>";
   let manufacturerAndModelRow = "<tr><td colspan='2' class='sign-cell'>" + manufacturer + " " + model + "</td></tr>";
   let storageRow = "<tr><td colspan='2' class='sign-cell'>" + storage + "</td></tr>";
   let carrierRow = "<tr><td colspan='2' class='sign-cell'>" + carrier + "</td></tr>";
