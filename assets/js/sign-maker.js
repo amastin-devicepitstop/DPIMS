fd10217aa0d487a89ba954f2569ca1662a457996
amastin-devicepitstop <43486447+amastin-devicepitstop@users.noreply.github.com>
2018-10-17 13:56:01 -0500

    Update sign-maker.js

--- assets/js/sign-maker.js
+++ assets/js/sign-maker.js
@@ -36,7 +36,7 @@ function toQueue() {
     let carrier = $(".sign-carrier").val();
     let storage = $(".sign-storage").val();
     let price = $(".sign-price").val();
-    let comments = '"' + $(".inputComment").val() '"';
+    let comments = $(".inputComment").val();
     let sku = $(".inputSKU").val();
   
     save("signs", sku, {product: product, carrier: carrier, storage: storage, price: price, comments: comments, sku: sku});
@@ -52,7 +52,7 @@ function addSign() {
   let carrier = $("#carrier").val();
   let storage = $("#storage").val();
   let price = $("#price").val();
-  let comments = $("#comments").val();
+  let comments = '"' + $("#comments").val() + '"';
   let sku = $("#sku").val();
   let id = '"' + 'id-' + sku + '"'
   
