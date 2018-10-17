833400389eca1244f95011ef0b50cd18f413c3d6
amastin-devicepitstop <43486447+amastin-devicepitstop@users.noreply.github.com>
2018-10-16 19:29:23 -0500

    Update sign-maker.js

--- assets/js/sign-maker.js
+++ assets/js/sign-maker.js
@@ -27,10 +27,11 @@ function addSign() {
   let price = $("#price").val();
   let comments = $("#comments").val();
   let sku = $("#sku").val();
+  let id = "product-" + sku; 
 
   let signBegin = "<div class='sign-preview'><div><table><tbody>";
   let logoAndPriceRow = "<tr><td class='dp-logo sign-logo'></td><td class='sign-cell'><input class='sign-price sign-value' value=" + '$' + price + "></td></tr>";
-  let manufacturerAndModelRow = "<tr><td colspan='2' class='sign-cell'><input class='sign-cell sign-value' value=" + manufacturer + ' ' + model + "></td></tr>";
+  let manufacturerAndModelRow = "<tr><td colspan='2' class='sign-cell'><input id=" + id + " class='sign-cell sign-value' value='' ></td></tr>";
   console.log(manufacturerAndModelRow);
   let storageRow = "<tr><td colspan='2' class='sign-cell'><input class='sign-cell sign-value' value=" + storage + "></input></td></tr>";
   let carrierRow = "<tr><td colspan='2' class='sign-cell'><input class='sign-cell sign-value' value=" + carrier + "></td></tr>";
@@ -64,6 +65,7 @@ function addSign() {
   
   count += 1;
   $(".page").append(sign);
+  
   console.log(count);
 }
 
