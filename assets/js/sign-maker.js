04e1eeaff58f6413cbabc6fb85f7a22f5ee8ee04
amastin-devicepitstop <43486447+amastin-devicepitstop@users.noreply.github.com>
2018-10-19 03:13:03 -0500

    Update sign-maker.js

--- assets/js/sign-maker.js
+++ assets/js/sign-maker.js
@@ -6,3 +6,8 @@ window.onload = function(){
 function emptyQueue() {
   showConfirmDialog("Do you want to delete all signs in the print queue?");
 }
+
+function removePendingSigns() {
+  closeModal();
+  removeAll('signs');
+}
