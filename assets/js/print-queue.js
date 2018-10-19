895f024e91d7738b4e60e8f5e2ecedca10c2545a
amastin-devicepitstop <43486447+amastin-devicepitstop@users.noreply.github.com>
2018-10-18 20:01:27 -0500

    Update print-queue.js

--- assets/js/print-queue.js
+++ assets/js/print-queue.js
@@ -73,6 +73,7 @@ function clearQueue() {
 
 function setupDelete() {
   $(".sign-delete").click(function() {
-    $(this).closest(".sign-delete").remove();
+    console.log($(this).closest(".sign-delete"));
+    //$(this).closest(".sign-delete").remove();
   });
 }
