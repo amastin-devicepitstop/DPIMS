a9e17360b7b621e9d494935bc263164e5950c185
amastin-devicepitstop <43486447+amastin-devicepitstop@users.noreply.github.com>
2018-10-19 03:00:07 -0500

    Update print-queue.js

--- assets/js/print-queue.js
+++ assets/js/print-queue.js
@@ -38,7 +38,7 @@ function createSign(sign, odd) {
   
   let signBegin = "<div class='sign-preview'><div class='sign-delete'></div><div><table><tbody>";
   let logoAndPriceRow = "<tr><td class='dp-logo sign-logo'></td><td class='sign-cell'><input class='sign-price sign-value' value=" + price + "></td></tr>";
-  let manufacturerAndModelRow = "<tr><td colspan='2' class='sign-cell'><input class='sign-product sign-cell sign-value' value=" + product + "></td></tr>";
+  let manufacturerAndModelRow = "<tr><td colspan='2' class='sign-cell'><input class='sign-product sign-cell sign-value'></td></tr>";
   let storageRow = "<tr><td colspan='2' class='sign-cell'><input class='sign-storage sign-cell sign-value' value=" + storage + "></input></td></tr>";
   let carrierRow = "<tr><td colspan='2' class='sign-cell'><input class='sign-carrier sign-cell sign-value' value=" + carrier + "></td></tr>";
   let commentsRow;
@@ -46,11 +46,11 @@ function createSign(sign, odd) {
   let skuRow = "<tr><td colspan='2' class='sign-cell sign-sku'><input class='sign-cell sign-value sign-sku inputSKU' value=" + sku + "></td></tr>";
   // If the sign has no comments, it should appear as a blank line
   if (comments == "" || comments == '""') {
-    commentsRow = "<tr><td colspan='2' class='sign-cell'><input class='sign-cell sign-value inputComment' oninput='inputChange($(this))' value=" + comments + "></td></tr>";
+    commentsRow = "<tr><td colspan='2' class='sign-cell'><input class='sign-cell sign-value inputComment' oninput='inputChange($(this))'></td></tr>";
   }
   // If the sign does have comments, it should appear as a yellow line with red text.
   else {
-    commentsRow = "<tr><td colspan='2' class='sign-cell sign-comment'><input class='sign-cell sign-value sign-comment inputComment' oninput='inputChange($(this))' value=" + comments + "></td></tr>";
+    commentsRow = "<tr><td colspan='2' class='sign-cell sign-comment'><input class='sign-cell sign-value sign-comment inputComment' oninput='inputChange($(this))'></td></tr>";
   }
   let signEnd = "</tbody></table></div></div>";
   let newSign = signBegin + logoAndPriceRow + manufacturerAndModelRow + storageRow + carrierRow + commentsRow + skuRow + signEnd
