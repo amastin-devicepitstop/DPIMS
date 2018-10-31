43d571859e105a70a5913496158ed435e6189ba4
amastin-devicepitstop <43486447+amastin-devicepitstop@users.noreply.github.com>
2018-10-31 12:01:05 -0500

    Create returns-tracker-new.js

---
+++ assets/js/returns-tracker-new.js
@@ -0,0 +1,34 @@
+window.onload = function(){
+  hideAnimation();
+  preventFormSubmit();
+  initDatabase();
+  autocomplete();
+}
+
+function preventFormSubmit() {
+  // Check that the current page is the "New Store Stock" page
+  
+  if (urlContains("new")) {
+    $("#date").val(new Date().toLocaleDateString());
+    $("#date").datepicker();
+    
+    // Prevent form submission
+    $("form").submit(function (e) {  
+      e.preventDefault();
+      addReturn();
+    });	
+  }
+}
+
+function addReturn() {
+  let name = document.getElementById("first-name").value + " " + document.getElementById("last-name").value;
+  let reason = document.getElementById("reason").value;
+  let sku = document.getElementById("sku").value;
+  let date = document.getElementById("date").value;
+  
+  let month = getMonth(date);
+  let day = getDay(date);
+  let year = getYear(date);
+
+  save("returns", sku, {tech: name, reason: reason, sku: sku, date: date, month: month, day: day, year: year});
+}
