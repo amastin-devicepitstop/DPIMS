736a9702725196ab6430c9a0fe173c09c71155b8
amastin-devicepitstop <43486447+amastin-devicepitstop@users.noreply.github.com>
2018-11-01 16:36:43 -0500

    Update storestock-edit.js

--- assets/js/storestock-edit.js
+++ assets/js/storestock-edit.js
@@ -70,10 +70,7 @@ function addStoreStock() {
   let sold = $("#sold").prop('checked');
   let ready = $("#ready").prop('checked');
   
-  console.log(sold);
-  console.log(ready);
-  
-  //save("devices", sku, {tech: name, manufacturer: manufacturer, model: model, actions: actions, sku: sku, date: date, month: month, day: day, year: year, sold: sold, ready: ready});
+  save("devices", sku, {tech: name, manufacturer: manufacturer, model: model, actions: actions, sku: sku, date: date, month: month, day: day, year: year, sold: sold, ready: ready});
 }
 
 function addAction() {
