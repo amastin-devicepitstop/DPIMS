f7f0aa78762b400de74c5465fc97fac27932387d
amastin-devicepitstop <43486447+amastin-devicepitstop@users.noreply.github.com>
2018-11-06 17:55:03 -0600

    Update buy-sale-tracker.js

--- assets/js/buy-sale-tracker.js
+++ assets/js/buy-sale-tracker.js
@@ -65,8 +65,8 @@ function addBuySale(product) {
   let nameDiv = "<div class='transaction-technician'><span class='block'>" + product.tech + "</span></div>";
   let skuDiv = "<div class='transaction-sku'><span class='block'>" + product.sku + "</span></div>";
   let date = "<div class='transaction-date'><label class='text-muted'>" + product.date + "</label></div>";
+  let buttonDiv = "<div class='right'><div><button type='button'>" + 'X' + "</button></div></div>";
   let id = "<div class='transaction-id'><label class='no-display'>" + product.id + "</label></div>";
-  let buttonDiv = "<div class='right'><div><button type='button'>" + 'X' + "</button></div></div>
   let divEnd = "</div></div>";
        
   let transaction = divStart + checkboxDiv + infoDivStart + productDiv + nameDiv + skuDiv + date +  buttonDiv + id + divEnd;     
