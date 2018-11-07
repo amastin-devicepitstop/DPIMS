ba17b9670c5492545edceb5d4c46317c9bda6e95
amastin-devicepitstop <43486447+amastin-devicepitstop@users.noreply.github.com>
2018-11-07 00:24:02 -0600

    Update buy-sale-tracker.js

--- assets/js/buy-sale-tracker.js
+++ assets/js/buy-sale-tracker.js
@@ -86,7 +86,7 @@ function initCheckboxes() {
   
   $(":checkbox").change(function() {
     
-    if ($.contains($("input:checkbox:checked"), $("#buys")){
+    if ($.contains($("input:checkbox:checked"), $("#buys"))){
       // If no checkboxes are selected, display "Store Stock Tracker"
       if ($("#buys input:checkbox:checked").length == 0) {
           $(".actions")[0].html(buys);
