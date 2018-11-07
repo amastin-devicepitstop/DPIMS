29598aa44ffb7e79885b31b04bedf48ad540fc54
amastin-devicepitstop <43486447+amastin-devicepitstop@users.noreply.github.com>
2018-11-07 14:01:27 -0600

    Update buy-sale-tracker.js

--- assets/js/buy-sale-tracker.js
+++ assets/js/buy-sale-tracker.js
@@ -100,9 +100,8 @@ function deleteBuySale(element) {
   let column = element.closest(".transaction-column")[0];
   let selected = $(column).find(".transaction-column-content input:checkbox:checked");
   let ids = $(column).find(".transaction-column-content input:checkbox:checked").closest(".transaction-list-item").find(".transaction-id label");
-//   let ids = $("input:checkbox:checked").closest(".transaction-list-item").find(".transaction-id label");
-//   let column = $("input:checkbox:checked").closest(".transaction-column-content");
   console.log(selected);
+  console.log(ids);
   
   for (let i = 0; i < selected.length; i++) {
 //     console.log(column);
