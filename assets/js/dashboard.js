e7d4ef508bcafead3010eb2e9808caa989f3bc2f
amastin-devicepitstop <43486447+amastin-devicepitstop@users.noreply.github.com>
2018-11-30 13:41:19 -0600

    Update dashboard.js

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -17,7 +17,7 @@ function editMSRP() {
   let val = "";
   for (let i = 0; i < span.length; i++) {
     val = span[i].innerText;
-    span[i].replaceWith("<input type='text' value=val></input">)
+    span[i].replaceWith("<input type='text' value=val></input>");
     console.log(val);
   }
 }
